7c73d6b303caa3218479c43bc75719dfa274a51a
TRustHvH <vpolakov88@gmail.com>
2023-04-19 02:11:00 +0300

    updated

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+function hoveropenother(){
+    document.getElementById("start-footer-first").classList.add("active")
+    document.getElementById("start-footer-second").classList.add("active")
+    document.getElementById("start-footer-third").classList.add("active")
+    document.getElementById("start-footer-fourth").classList.add("active")
+}
+function hovercloseother(){
+    document.getElementById("start-footer-first").classList.remove("active")
+    document.getElementById("start-footer-second").classList.remove("active")
+    document.getElementById("start-footer-third").classList.remove("active")
+    document.getElementById("start-footer-fourth").classList.remove("active")
+}
